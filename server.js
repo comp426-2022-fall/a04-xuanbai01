7db5ad47cc2684d87d0019be2b5fc6b47e56a39f
shawnbai <xuanbai@ad.unc.edu>
2022-11-30 00:43:27 -0500

    commit03

--- server.js
+++ server.js
@@ -18,35 +18,41 @@ const port = args.port || 5000;
   });
 
   app.get("/app/roll/", (req, res) => {
-    console.log(roll(6, 2, 1));
-    res.send(roll(6, 2, 1));
+    let sides = 6;
+    let dice = 2;
+    let rolls = 1;
+    console.log(roll(sides, dice, rolls));
+    res.send(roll(sides, dice, rolls));
   });
 
   app.post('/app/roll/', (req, res) => {
-    sides = parseInt(req.body.sides);
-    dice = parseInt(req.body.dice);
-    const rolls = parseInt(req.body.rolls);
+    let sides = parseInt(req.body.sides);
+    let dice = parseInt(req.body.dice);
+    let rolls = parseInt(req.body.rolls);
     console.log(roll(sides, dice, rolls));
     res.send(roll(sides, dice, rolls));
   });
 
   app.get('/app/roll/:sides/', (req, res) => {
-    const sides = parseInt(req.params.sides);
-    console.log(roll(sides, 2, 1));
-    res.send(roll(sides, 2, 1));
+    let sides = parseInt(req.params.sides);
+    let dice = 2;
+    let rolls = 1;
+    console.log(roll(sides, dice, rolls));
+    res.send(roll(sides, dice, rolls));
   });
 
   app.get('/app/roll/:sides/:dice/', (req, res) => {
-    sides = parseInt(req.params.sides);
-    dice = parseInt(req.params.dice);
-    console.log(roll(sides, dice, 1));
-    res.send(roll(sides, dice, 1));
+    let sides = parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
+    let rolls = 1;
+    console.log(roll(sides, dice, rolls));
+    res.send(roll(sides, dice, rolls));
   });
 
   app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
-    sides =  parseInt(req.params.sides);
-    dice = parseInt(req.params.dice);
-    const rolls = parseInt(req.params.rolls);
+    let sides =  parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
+    let rolls = parseInt(req.params.rolls);
     console.log(roll(sides, dice, rolls));
     res.send((roll(sides, dice, rolls)));
   });
